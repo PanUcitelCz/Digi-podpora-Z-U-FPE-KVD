885054af485a6771cb2976f90aa5b2d0a8c2bb83
PanUcitelCz <104927244+PanUcitelCz@users.noreply.github.com>
2023-11-08 13:53:57 +0100

    Aplikace bez backendu

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+	import '../lib/css/normallize.css';
+</script>
+
+<Header />
+<main>
+	<slot />
+</main>
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@10..48,200;10..48,600&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<style lang="stylus">
+	
+	body
+		margin 0
+		padding 0
+
+	main
+		display flex
+		justify-content center
+		margin 30px 0
+
+</style>
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	css: {
+		preprocessorOptions: {
+			stylus: {
+				imports: [path.resolve(__dirname, 'src/lib/css/global.styl')]
+			}
+		}
+	}
+});
